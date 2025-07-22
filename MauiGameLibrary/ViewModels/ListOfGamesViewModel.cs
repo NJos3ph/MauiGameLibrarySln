aacefc35363b110195a172929e4656354249fddb
NJos3ph <nathanjoseph051@gmail.com>
2025-07-22 14:26:32 +0200

    Add project files.

---
+++ MauiGameLibrary/ViewModels/ListOfGamesViewModel.cs
@@ -0,0 +1,46 @@
+﻿using MauiGameLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiGameLibrary.Models;
+using MauiGameLibrary.Services;
+
+
+
+namespace MauiGameLibrary.ViewModels
+{
+    public class ListOfGamesViewModel : BaseViewModel
+    {
+        private GameDataService _gameDataService;
+
+        private List<GameInformation> _games;
+
+        public List<GameInformation> Games
+        {
+
+            get { return _games; }
+            set { _games = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+
+        public ListOfGamesViewModel(GameDataService service)
+        {
+            _gameDataService = service;
+
+        }
+
+        public void RefreshGames()
+        {
+
+            Games = _gameDataService.GetAllGameInformation();
+
+        }
+
+
+    }
+}
---
+++ MauiGameLibrary/Views/ListOfGamesView.xaml.cs
@@ -0,0 +1,29 @@
+using MauiGameLibrary.ViewModels;
+
+namespace MauiGameLibrary.Views;
+
+public partial class ListOfGamesView : ContentPage
+{
+	public ListOfGamesView(ListOfGamesViewModel vm)
+	{
+		InitializeComponent();
+
+		BindingContext = vm;
+
+		
+	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+		ListOfGamesViewModel vm = BindingContext as ListOfGamesViewModel;
+
+		if (vm != null)
+		{
+
+			vm.RefreshGames();
+		}
+    }
+
+}
